a0794ca37da1531d26a90990172ff44d650e8742
Felichita <42844486+semenovfilipp@users.noreply.github.com>
2022-11-13 17:26:27 +0300

    Задания 1.1 "Основные типы данных"

---
+++ src/Five.java
@@ -0,0 +1,11 @@
+//Дано целое число n. Выведите следующее за ним четное число.
+// При решении этой задачи нельзя использовать условную инструкцию if и циклы.
+//Подсказка: Подумайте, что будет оставаться в остатке при делении на 2 у чётных и у нечётных чисел.
+import java.util.Scanner;
+public class Five {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n + 2 - (n % 2));
+    }
+}
---
+++ src/One.java
@@ -0,0 +1,14 @@
+////Создайте две переменные: целого типа a и действительного b, равные 5 и 0.4 соответственно.
+///Выведите построчно на экран их сумму, разность, произведение и частное.
+
+
+public class One {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 0.4;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
